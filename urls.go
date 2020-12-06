c21adffad0ab16b55b68890e4fb0dec771cfdf93
umisan <t.umino.nit@gmail.com>
2020-12-06 21:58:56 +0900

    ルートもルーティング

--- urls.go
+++ urls.go
@@ -1,6 +1,7 @@
 package main
 
 var urls []Url = []Url{
+	{Path: "/", Method: "GET", View: createIndexView()},
 	{Path: "/index", Method: "GET", View: createIndexView()},
 	{Path: "/index", Method: "POST", View: createPostIndexView()},
 }
